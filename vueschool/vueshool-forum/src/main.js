eb6e419cc2b74a1915f4b918fbbfc049459fb29e
Pedro Palazón <palazon@gmail.com>
2019-06-24 21:00:36 +0200

    Vue 2 MasterClass - Cap 54,55 Configuring Firebase

--- vueschool/vueshool-forum/src/main.js
+++ vueschool/vueshool-forum/src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import firebase from 'firebase'
 import App from './App'
 import router from './router'
 import store from '@/store'
@@ -10,6 +11,18 @@ Vue.component('AppDate', AppDate)
 
 Vue.config.productionTip = false
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAYmlGZzRcMwDjo8HG9YhwJJzMbyyNPXus',
+  authDomain: 'vue-school-forum-4bd38.firebaseapp.com',
+  databaseURL: 'https://vue-school-forum-4bd38.firebaseio.com',
+  projectId: 'vue-school-forum-4bd38',
+  storageBucket: '',
+  messagingSenderId: '918573349399',
+  appId: '1:918573349399:web:62c793e0cad8ff32'
+}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
