2d2711caf5484d2a7e517b64a195b795fbe53342
Pedro Palazón <palazon@gmail.com>
2019-09-23 14:34:35 +0200

    no message

--- vueschool/vueshool-forum/src/pages/PageHome.vue
+++ vueschool/vueshool-forum/src/pages/PageHome.vue
@@ -16,6 +16,28 @@ export default {
     categories () {
       return Object.values(this.$store.state.categories)
     }
+  },
+  beforeCreate () {
+    // best places to AJAX functions
+    console.log('📡 beforeCreate', this.categories)
+  },
+  created () {
+    console.log('📡 created', this.categories)
+  },
+  beforeMount () {
+    console.log('📡 beforeMount', this.categories)
+  },
+  mounted () {
+    // Mount virutal DOM into DOM
+    console.log('📡 mounted', this.categories, this.$el.innerText)
+  },
+  beforeDestroy () {
+    // best place to turn off listeners
+    console.log('📡 beforeDestroy .turn off listesners', this.categories, this.$el.innerText)
+  },
+  destroyed () {
+    // not access to $el properties
+    console.log('📡 destroyed', this.categories)
   }
 }
 </script>
